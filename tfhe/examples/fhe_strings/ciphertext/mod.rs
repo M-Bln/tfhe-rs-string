55c370a9eb7f574d06ad93b294be017837f3dbad
M-Bln <mlnd2@protonmail.com>
2023-11-06 12:05:38 +0100

    create type FheString encrypt string to FheString

---
+++ tfhe/examples/fhe_strings/ciphertext/mod.rs
@@ -0,0 +1,48 @@
+use tfhe::integer::{gen_keys_radix, RadixCiphertext, RadixClientKey, ServerKey};
+use tfhe::shortint::parameters::PARAM_MESSAGE_2_CARRY_2_KS_PBS;
+
+
+pub struct FheAsciiChar(RadixCiphertext);
+
+pub type FheString = Vec<FheAsciiChar>;
+
+
+pub fn encrypt_str(
+    client_key: &RadixClientKey,
+    s: &str,
+) -> Result<FheString, Box<dyn std::error::Error>> {
+    if !s.is_ascii() {
+	return Err("content contains non-ascii characters".into())
+    }
+    Ok(s.as_bytes()
+        .iter()
+        .map(|byte| FheAsciiChar(client_key.encrypt(*byte as u64)))
+        .collect())
+}
+
+pub fn decrypt_fhe_string(
+    client_key: &RadixClientKey,
+    s: &FheString,
+) -> Result<String, Box<dyn std::error::Error>> {
+    Ok(String::from_utf8(s.iter()
+		       .map(|cipher_char| client_key.decrypt::<u8>(&cipher_char.0))
+		       .collect())?)
+}
+
+pub fn gen_keys() -> (RadixClientKey, ServerKey){
+    let num_block = 4;
+    gen_keys_radix(PARAM_MESSAGE_2_CARRY_2_KS_PBS, num_block)
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::ciphertext::gen_keys;
+    use crate::ciphertext::encrypt_str;
+
+    #[test]
+    fn test_encrypt() {
+	let (client_key, _) = gen_keys();
+
+	let _ = encrypt_str(&client_key, "Hello world!");	
+    }
+}
