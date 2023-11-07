fd0299e10df1e63221cfc8763762edfdc97d2bf6
Juul-Mc-Goa <jules.magois.pro@proton.me>
2023-11-07 21:37:28 +0000

    create keys only once for all tests

--- tfhe/examples/fhe_strings/ciphertext/mod.rs
+++ tfhe/examples/fhe_strings/ciphertext/mod.rs
@@ -38,7 +38,7 @@ pub fn decrypt_fhe_string(
     client_key: &RadixClientKey,
     s: &FheString,
 ) -> Result<String, FromUtf8Error> {
-    Ok(String::from_utf8(decrypt_fhe_ascii_vec(client_key, s))?)
+    String::from_utf8(decrypt_fhe_ascii_vec(client_key, s))
 }
 
 pub fn gen_keys() -> (RadixClientKey, ServerKey) {
@@ -74,21 +74,25 @@ pub fn null_padded_utf8_from_str(s: &str, length: usize) -> Vec<u8> {
 #[cfg(test)]
 mod tests {
     use crate::ciphertext::{encrypt_ascii_vec, gen_keys};
+    use lazy_static::lazy_static;
+    use tfhe::integer::{RadixClientKey, ServerKey};
+
+    lazy_static! {
+        pub static ref KEYS: (RadixClientKey, ServerKey) = gen_keys();
+    }
 
     #[test]
     fn test_encrypt_ascii_vec() {
-        let (client_key, _) = gen_keys();
-        assert!(encrypt_ascii_vec(&client_key, &vec![0, 0, 97, 98, 99, 100, 0]).is_ok());
-        assert!(encrypt_ascii_vec(&client_key, &vec![0, 0, 0xc3, 0x28, 0, 0]).is_err());
+        assert!(encrypt_ascii_vec(&KEYS.0, &vec![0, 0, 97, 98, 99, 100, 0]).is_ok());
+        assert!(encrypt_ascii_vec(&KEYS.0, &vec![0, 0, 0xc3, 0x28, 0, 0]).is_err());
     }
 
     use crate::ciphertext::decrypt_fhe_ascii_vec;
 
     #[test]
     fn test_decrypt_encrypt_ascii_vec() {
-        let (client_key, _) = gen_keys();
-        let encrypted_s = encrypt_ascii_vec(&client_key, &vec![0, 0, 97, 98, 99, 100, 0]).unwrap();
-        let decrypted_s = decrypt_fhe_ascii_vec(&client_key, &encrypted_s);
+        let encrypted_s = encrypt_ascii_vec(&KEYS.0, &vec![0, 0, 97, 98, 99, 100, 0]).unwrap();
+        let decrypted_s = decrypt_fhe_ascii_vec(&KEYS.0, &encrypted_s);
         println!("the decrypted vec is \"{:?}\"", decrypted_s);
         println!("it is expected to be \"[0,0,97,98,99,100,0]\"");
         assert_eq!(decrypted_s, vec![0, 0, 97, 98, 99, 100, 0]);
@@ -98,18 +102,16 @@ mod tests {
 
     #[test]
     fn test_encrypt() {
-        let (client_key, _) = gen_keys();
-        assert!(encrypt_str(&client_key, "Hello world!").is_ok())
+        assert!(encrypt_str(&KEYS.0, "Hello world!").is_ok())
     }
 
     use crate::ciphertext::decrypt_fhe_string;
 
     #[test]
     fn test_decrypt_encrypt() {
-        let (client_key, _) = gen_keys();
         let plain_text = "abc";
-        let encrypted_str = encrypt_str(&client_key, plain_text).unwrap();
-        let decrypted_str = decrypt_fhe_string(&client_key, &encrypted_str).unwrap();
+        let encrypted_str = encrypt_str(&KEYS.0, plain_text).unwrap();
+        let decrypted_str = decrypt_fhe_string(&KEYS.0, &encrypted_str).unwrap();
         println!(
             "the decrypted string is \"{}\", it is expected to be \"{}\"",
             decrypted_str, plain_text,
