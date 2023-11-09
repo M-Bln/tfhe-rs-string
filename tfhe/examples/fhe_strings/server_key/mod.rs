3e81cde5e8a95e706a4a9f98b9c0f61692650131
M-Bln <mlnd2@protonmail.com>
2023-11-09 15:52:22 +0100

    FheString now wraps Padding and Length The Length is either clear or crypted

--- tfhe/examples/fhe_strings/server_key/mod.rs
+++ tfhe/examples/fhe_strings/server_key/mod.rs
@@ -3,13 +3,11 @@ mod change_case;
 use tfhe::integer::ServerKey;
 
 pub struct StringServerKey {
-    pub integer_key : ServerKey,
+    pub integer_key: ServerKey,
 }
 
 impl From<ServerKey> for StringServerKey {
     fn from(integer_key: ServerKey) -> Self {
-	Self {
-	    integer_key,
-	}
+        Self { integer_key }
     }
 }
