dd2d7654cba1ff7ed7894b741d2af105b7a09e6f
M-Bln <mlnd2@protonmail.com>
2023-11-10 16:22:50 +0100

    trim_start a clear or crypted char

--- tfhe/examples/fhe_strings/server_key/trim.rs
+++ tfhe/examples/fhe_strings/server_key/trim.rs
@@ -45,7 +45,7 @@ impl StringServerKey {
         *length = self.add_radix_length(&length, &encrypted_int);
     }
 
-    pub fn trim_start_no_init_padding(&self, s: &FheString) -> FheString {
+    pub fn trim_start_clear_char_no_padding(&self, s: &FheString, character: u8) -> FheString {
         let mut continue_triming = self.create_true();
         let mut result_content: Vec<FheAsciiChar> = Vec::with_capacity(s.content.len());
         let mut result_length: FheStrLength = s.length.clone();
@@ -53,9 +53,36 @@ impl StringServerKey {
         for c in s.content.iter() {
             self.integer_key.bitand_assign_parallelized(
                 &mut continue_triming,
-                &self
-                    .integer_key
-                    .scalar_eq_parallelized(&c.0, ASCII_WHITE_SPACE),
+                &self.integer_key.scalar_eq_parallelized(&c.0, character),
+            );
+            self.add_assign_radix_length(&mut result_length, &continue_triming);
+            result_content.push(FheAsciiChar(self.integer_key.cmux_parallelized(
+                &continue_triming,
+                &self.integer_key.create_trivial_zero_radix(4),
+                &c.0,
+            )))
+        }
+
+        FheString {
+            content: result_content,
+            padding: Padding::InitialAndFinal,
+            length: result_length,
+        }
+    }
+
+    pub fn trim_start_encrypted_char_no_padding(
+        &self,
+        s: &FheString,
+        encrypted_char: &FheAsciiChar,
+    ) -> FheString {
+        let mut continue_triming = self.create_true();
+        let mut result_content: Vec<FheAsciiChar> = Vec::with_capacity(s.content.len());
+        let mut result_length: FheStrLength = s.length.clone();
+
+        for c in s.content.iter() {
+            self.integer_key.bitand_assign_parallelized(
+                &mut continue_triming,
+                &self.integer_key.eq_parallelized(&c.0, &encrypted_char.0),
             );
             self.add_assign_radix_length(&mut result_length, &continue_triming);
             result_content.push(FheAsciiChar(self.integer_key.cmux_parallelized(
@@ -75,7 +102,7 @@ impl StringServerKey {
 
 #[cfg(test)]
 mod tests {
-    use crate::ciphertext::{decrypt_fhe_string, encrypt_str, gen_keys};
+    use crate::ciphertext::{decrypt_fhe_string, encrypt_str, gen_keys, FheAsciiChar};
     use crate::server_key::StringServerKey;
     use lazy_static::lazy_static;
     use tfhe::integer::RadixClientKey;
@@ -85,10 +112,23 @@ mod tests {
     }
 
     #[test]
-    fn test_trim_start_no_init_padding() {
-        let encrypted_str = encrypt_str(&KEYS.0, " a").unwrap();
-        let trimed_encrypted_str = KEYS.1.trim_start_no_init_padding(&encrypted_str);
+    fn test_trim_start_clear_char_no_padding() {
+        let encrypted_str = encrypt_str(&KEYS.0, "ab").unwrap();
+        let trimed_encrypted_str = KEYS
+            .1
+            .trim_start_clear_char_no_padding(&encrypted_str, b'a');
         let decrypted_str = decrypt_fhe_string(&KEYS.0, &trimed_encrypted_str).unwrap();
-        assert_eq!(&decrypted_str, "a");
+        assert_eq!(&decrypted_str, "b");
+    }
+
+    #[test]
+    fn test_trim_start_encrypted_char_no_padding() {
+        let encrypted_str = encrypt_str(&KEYS.0, "ab").unwrap();
+        let encrypted_char = FheAsciiChar(KEYS.0.encrypt(b'a'));
+        let trimed_encrypted_str = KEYS
+            .1
+            .trim_start_encrypted_char_no_padding(&encrypted_str, &encrypted_char);
+        let decrypted_str = decrypt_fhe_string(&KEYS.0, &trimed_encrypted_str).unwrap();
+        assert_eq!(&decrypted_str, "b");
     }
 }
