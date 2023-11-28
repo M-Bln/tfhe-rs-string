21b1505ec0e76bd6dc9f052730e2fcd10bb8fffb
M-Bln <mlnd2@protonmail.com>
2023-11-28 11:28:18 +0100

    Documentation for accessors and substring

--- tfhe/examples/fhe_strings/server_key/repeat.rs
+++ tfhe/examples/fhe_strings/server_key/repeat.rs
@@ -1,5 +1,6 @@
 use crate::ciphertext::{ClearOrEncrypted, FheAsciiChar, FheStrLength, FheString, Padding};
 use crate::server_key::StringServerKey;
+use tfhe::integer::RadixCiphertext;
 
 impl StringServerKey {
     pub fn repeat_clear(&self, s: &FheString, n: usize) -> FheString {
@@ -12,6 +13,52 @@ impl StringServerKey {
             result = self.add_encrypted(result, s);
         }
         result
+    }
+
+    pub fn repeat_encrypted(&self, s: &FheString, n: &RadixCiphertext, n_max: usize) -> FheString {
+        let zero = self.create_zero();
+
+        let result_padding = match s.padding {
+            Padding::None => Padding::None,
+            _ => Padding::Anywhere,
+        };
+
+        let result_length = self.radix_multiply_length(&s.length, n);
+        let mut result_content = Vec::with_capacity(s.content.len() * n_max);
+
+        for i in 0..n_max {
+            for c in &s.content {
+                let repeat_again: RadixCiphertext =
+                    self.integer_key.scalar_gt_parallelized(&n, i as u64);
+                let new_encrypted_char = FheAsciiChar(self.integer_key.cmux_parallelized(
+                    &repeat_again,
+                    &c.0,
+                    &zero,
+                ));
+                result_content.push(new_encrypted_char);
+            }
+        }
+        FheString {
+            padding: result_padding,
+            length: result_length,
+            content: result_content,
+        }
+    }
+
+    pub fn radix_multiply_length(
+        &self,
+        length: &FheStrLength,
+        n: &RadixCiphertext,
+    ) -> FheStrLength {
+        match &length {
+            ClearOrEncrypted::Clear(clear_length) => ClearOrEncrypted::Encrypted(
+                self.integer_key
+                    .scalar_mul_parallelized(n, *clear_length as u64),
+            ),
+            ClearOrEncrypted::Encrypted(encrypted_length) => {
+                ClearOrEncrypted::Encrypted(self.integer_key.mul_parallelized(n, encrypted_length))
+            }
+        }
     }
 }
 
@@ -35,5 +82,14 @@ mod tests {
         let repeated_s = SERVER_KEY.repeat_clear(&s, 3);
         let decrypted_result = CLIENT_KEY.decrypt_string(&repeated_s).unwrap();
         assert_eq!(decrypted_result, "ababab");
+    }
+
+    #[test]
+    fn test_repeat_encrypted() {
+        let s = CLIENT_KEY.encrypt_str_random_padding("a", 1).unwrap();
+        let encrypted_n = SERVER_KEY.create_n(2);
+        let repeated_s = SERVER_KEY.repeat_encrypted(&s, &encrypted_n, 2);
+        let decrypted_result = CLIENT_KEY.decrypt_string(&repeated_s).unwrap();
+        assert_eq!(decrypted_result, "aa");
     }
 }
