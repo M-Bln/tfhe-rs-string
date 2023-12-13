5eb046d3bbda3a1de17b29e80e34a9556b3226a1
M-Bln <mlnd2@protonmail.com>
2023-12-13 20:07:15 +0100

    time string methods with no arguments

--- tfhe/examples/fhe_strings/main.rs
+++ tfhe/examples/fhe_strings/main.rs
@@ -4,10 +4,12 @@ mod integer_arg;
 mod pattern;
 mod server_key;
 mod test_generating_macros;
+mod timing_macros;
 
 use crate::ciphertext::{gen_keys_test, FheString};
 use crate::client_key::StringClientKey;
 use crate::server_key::StringServerKey;
+//use crate::{time_function};
 use clap::Parser;
 use lazy_static::lazy_static;
 
@@ -33,7 +35,7 @@ struct Arguments {
     integer_arg: Option<usize>,
 
     /// Padding
-    #[clap(default_value_t = 0, short, long)]
+    #[clap(default_value_t = 2, short, long)]
     padding_zeros: usize,
 }
 
@@ -45,17 +47,102 @@ lazy_static! {
 
 fn main() {
     let arguments = Arguments::parse();
-    let encrypted_s = CLIENT_KEY.encrypt_str(&arguments.input_string).unwrap();
-    let mut fhe_result: FheString;
-    let mut clear_result: String;
-    println!(
-        "trim the encrypted string {:?} with no padding",
-        arguments.input_string
-    );
-    let start = std::time::Instant::now();
-    fhe_result = SERVER_KEY.trim(&encrypted_s);
-    clear_result = CLIENT_KEY.decrypt_string(&fhe_result).unwrap();
-    println!("fhe result: {:?}", clear_result);
-    println!("in: {:?}", start.elapsed());
-    println!("std result: {:?}", arguments.input_string.trim());
+    let clear_s = arguments.input_string;
+    let padding_zeros = arguments.padding_zeros;
+    let encrypted_s = CLIENT_KEY.encrypt_str(&clear_s).unwrap();
+    let encrypted_s_padding = CLIENT_KEY
+        .encrypt_str_padding(&clear_s, padding_zeros)
+        .unwrap();
+
+    macro_rules! time_function {
+        ($method: ident) => {
+            time_function_no_padding!($method, encrypted_s, clear_s);
+            time_function_padding!($method, encrypted_s_padding, clear_s, padding_zeros);
+        };
+    }
+
+    time_function!(trim);
+    time_function!(trim_start);
+    time_function!(trim_end);
+    time_function!(to_lowercase);
+    time_function!(to_uppercase);
+
+    // let mut fhe_result: FheString;
+    // let mut clear_result: String;
+    // println!("\n\nfunction: trim");
+    // println!("String argument: {:?} (no padding)\n", arguments.input_string);
+    // let start = std::time::Instant::now();
+    // fhe_result = SERVER_KEY.trim(&encrypted_s);
+    // clear_result = CLIENT_KEY.decrypt_string(&fhe_result).unwrap();
+    // let duration = start.elapsed();
+    // println!("FHE result:");
+    // println!("SERVER_KEY.trim(&encrypted_string) = {:?}\n", clear_result);
+    // println!("Computed in: {:?}\n", start.elapsed());
+    // println!("Rust standard library result:");
+    // println!("{:?}.trim() = {:?}\n", arguments.input_string, arguments.input_string.trim());
+    // println!(
+    //     "{0: <10} | {1: <10} | {2: <10} | {3: <10} ",
+    //     "Function", "string argument", "pattern", "second_pattern",
+    // );
+    // println!("\n\n\nfunction: trim");
+    // println!("Arguments:");
+    // println!("String argument: \n s = {:?};", arguments.input_string);
+    // println!("Encrypted string (no padding) \n &encrypted_s = CLIENT_KEY.encrypt_str({:?})",
+    // arguments.input_string); println!("{0: <40} = {1:? }", "str_arg.trim()",
+    // arguments.input_string.trim()); println!("{0: <40} = {1:? }",
+    // "SERVER_KEY.trim(&encrypted_str)", clear_result); println!("time:{:?} ", duration);
+
+    // println!("\n\n\n{0: <20} {1:}", "function:", "trim");
+    // println!("Arguments:");
+    // println!("{0: <35} {1:?}", " -Encrypted string (no padding)", arguments.input_string);
+    // println!("Results:");
+    // println!("{0: <35} {1:?}", " -std result:", arguments.input_string.trim() );
+    // println!("{0: <35} {1:?}", " -FHE result:", clear_result );
+    // println!("time:{:?} ", duration);
+
+    // #[macro_export]
+    // macro_rules! time_function_no_padding{
+    // 	($method: ident) => {
+    // 	    let start = std::time::Instant::now();
+    // 	    let fhe_result = SERVER_KEY.$method(&encrypted_s);
+    // 	    let clear_result = CLIENT_KEY.decrypt_string(&fhe_result).unwrap();
+    // 	    let duration = start.elapsed();
+    // 	    let std_result = clear_s.$method();
+    // 	    println!("\n\n\n{0: <20} {1:}", "function:", std::stringify!($method));
+    // 	    println!("Arguments:");
+    // 	    println!("{0: <35} {1:?}", " -Encrypted string", clear_s);
+    // 	    println!("  No padding");
+    // 	    println!("Results:");
+    // 	    println!("{0: <35} {1:?}", " -std result:", std_result);
+    // 	    println!("{0: <35} {1:?}", " -FHE result:", clear_result);
+    // 	    println!("time:{:?} ", duration);
+    // 	}
+    // };
+
+    // #[macro_export]
+    // macro_rules! time_function_padding{
+    // 	($method: ident) => {
+    // 	    let start = std::time::Instant::now();
+    // 	    let fhe_result = SERVER_KEY.$method(&encrypted_s_padding);
+    // 	    let clear_result = CLIENT_KEY.decrypt_string(&fhe_result).unwrap();
+    // 	    let duration = start.elapsed();
+    // 	    let std_result = clear_s.$method();
+    // 	    println!("\n\n\n{0: <20} {1:}", "function:", std::stringify!($method));
+    // 	    println!("Arguments:");
+    // 	    println!("{0: <35} {1:?}", " -Encrypted string", clear_s);
+    // 	    println!("  {0:} padding zeros", padding_zeros);
+    // 	    println!("Results:");
+    // 	    println!("{0: <35} {1:?}", " -std result:", std_result);
+    // 	    println!("{0: <35} {1:?}", " -FHE result:", clear_result);
+    // 	    println!("time:{:?} ", duration);
+    // 	}
+    // };
+
+    // #[macro_export]
+    // macro_rules! time_function{
+    // 	($method: ident) => {
+    // 	    time_function_no_padding!($method);
+    // 	    time_function_padding!($method);
+    // 	}
+    // };
 }
